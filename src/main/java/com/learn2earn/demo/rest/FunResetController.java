306d590a8cadc454a9659e83f8cb4701099e153a
Aditya Deshmukh <adityadeshmukh7350@gmail.com>
2023-09-01 22:25:59 +0530

    Created controller

---
+++ src/main/java/com/learn2earn/demo/rest/FunResetController.java
@@ -0,0 +1,14 @@
+package com.learn2earn.demo.rest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunResetController {
+
+    // expose "/" that return Hello World!
+
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello World!";
+    }
+}
